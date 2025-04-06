c84d5ddfb31acfcf1415d7a4e0dd4ce598f86a7f
SRubio <srubio@jabawaka.com>
2025-04-06 13:23:57 +0200

    Added editing of Sections and Tasks

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use eframe::egui;
+use eframe::egui::{self, Label, RichText, Sense, TextEdit, TextBuffer, ViewportBuilder};
 
 
 fn main() {
-    let native_options = eframe::NativeOptions::default();
+    let mut native_options = eframe::NativeOptions::default();
+    let viewport = ViewportBuilder::default().with_inner_size([300.0, 600.0]);
+    native_options.viewport = viewport;
+
     let _ = eframe::run_native("Yet Another ToDo",  native_options, Box::new(|cc| Ok(Box::new(MyApp::new(cc)))));
 }
 
@@ -10,17 +13,29 @@ fn main() {
 struct Task {
     text: String,
     done: bool,
+    edit: bool,
 }
 
 
 struct Section {
     title: String,
     tasks: Vec<Task>,
+    edit: bool,
+}
+
+
+enum Mode {
+    Idle,
+    EditTask,
+    EditSection,
+    AddTask,
+    AddSection,
 }
 
 
 struct MyApp {
     sections: Vec<Section>,
+    mode: Mode,
 }
 
 
@@ -28,6 +43,7 @@ impl MyApp {
     fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         let mut app = MyApp {
             sections: vec![],
+            mode: Mode::Idle,
         };
 
         app.add_section("Graphics");
@@ -43,7 +59,7 @@ impl MyApp {
     }
 
     fn add_section(&mut self, title: &str) {
-        self.sections.push(Section {title: title.to_string(), tasks: vec![]});
+        self.sections.push(Section {title: title.to_string(), tasks: vec![], edit: false});
         println!("Added section {}", title);
     }
 
@@ -51,7 +67,7 @@ impl MyApp {
         for section in &mut self.sections {
             if section_title == section.title {
                 println!("Added task {}", task);
-                section.tasks.push(Task {text: task.to_string(), done: false});
+                section.tasks.push(Task {text: task.to_string(), done: false, edit: false});
                 break;
             } else {
                 println!("Could not add task {}", task);
@@ -64,14 +80,93 @@ impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
 
-            for section in &mut self.sections {
-                ui.heading(section.title.clone());
+            match self.mode {
+                // In this mode you can click a Task or a Section to edit it,
+                // the checkboxes to mark tasks as completed and add new Tasks
+                // and Sections
+                Mode::Idle => {
+                    for section in &mut self.sections {
+                        if ui.add(Label::new(RichText::new(&section.title).heading()).sense(Sense::click())).clicked() {
+                            // Enter edit section mode
+                            section.edit = true;
+                            self.mode = Mode::EditSection;
+                        }
 
-                for task in &mut section.tasks {
-                    ui.checkbox(&mut task.done, task.text.clone());
-                }
+                        for task in &mut section.tasks {
+                            ui.horizontal(|ui| {
+                                ui.checkbox(&mut task.done, "");
+                                if ui.add(Label::new(&task.text).sense(Sense::click())).clicked() {
+                                    task.edit = true;
+                                    self.mode = Mode::EditTask;
+                                }
+                            });
+                        }
+
+                        ui.add_space(12.0);
+                    }
+                },
+
+                // In this mode all Sections and Tasks are rendered as plain
+                // labels except for the Task being edited which should be an
+                // edit box
+                Mode::EditTask => {
+                    for section in &mut self.sections {
+                        ui.heading(&section.title);
+
+                        for task in &mut section.tasks {
+                            ui.horizontal(|ui| {
+                                ui.checkbox(&mut task.done, "");
+                                if task.edit {
+                                    // Render edit text box for task
+                                    let response = ui.add(TextEdit::singleline(&mut task.text));
+
+                                    if response.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter)) {
+                                        self.mode = Mode::Idle;
+                                    }
+                                } else {
+                                    // Render normally
+                                    ui.label(&task.text);
+                                }
+                            });
+                        }
+
+                        ui.add_space(12.0);
+                    }
+                },
+
+                // In this mode all Sections and Tasks are rendered as plain
+                // labels except for the Section being edited which should be an
+                // edit box
+                Mode::EditSection => {
+                    for section in &mut self.sections {
+
+                        if section.edit {
+                            let response = ui.add(TextEdit::singleline(&mut section.title));
+
+                            if response.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter)) {
+                                self.mode = Mode::Idle;
+                            }
+                        } else {
+                            ui.heading(&section.title);
+                        }
+
+                        for task in &mut section.tasks {
+                            ui.horizontal(|ui| {
+                                ui.checkbox(&mut task.done, "");
+                                ui.label(&task.text);
+                            });
+                        }
+
+                        ui.add_space(12.0);
+                    }
+                },
+
+                Mode::AddTask => {
+                },
+
+                Mode::AddSection => {
+                },
             }
-
         });
     }
 }
