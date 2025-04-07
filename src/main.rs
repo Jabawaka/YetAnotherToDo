e2060966eb4396bcbdfe6cef7c5b016c7fa39723
Sergio Rubio <srubio@jabawaka.com>
2025-04-07 11:22:02 +0200

    Implemented adding Tasks and Sections

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use eframe::egui::{self, Label, RichText, Sense, TextEdit, TextBuffer, ViewportBuilder};
+use eframe::egui::{self, Label, RichText, Sense, TextEdit, ViewportBuilder};
 
 
 fn main() {
     let mut native_options = eframe::NativeOptions::default();
-    let viewport = ViewportBuilder::default().with_inner_size([300.0, 600.0]);
-    native_options.viewport = viewport;
+    //let viewport = ViewportBuilder::default().with_inner_size([300.0, 600.0]);
+    //native_options.viewport = viewport;
 
     let _ = eframe::run_native("Yet Another ToDo",  native_options, Box::new(|cc| Ok(Box::new(MyApp::new(cc)))));
 }
@@ -23,19 +23,25 @@ struct Section {
     edit: bool,
 }
 
+impl Section {
+    fn add_task(&mut self, task: &str, edit: bool) {
+        self.tasks.push(Task {text: task.to_string(), done: false, edit});
+    }
+}
+
 
 enum Mode {
     Idle,
     EditTask,
     EditSection,
-    AddTask,
-    AddSection,
 }
 
 
 struct MyApp {
     sections: Vec<Section>,
     mode: Mode,
+    first_time_edit: bool,
+    scale_factor: f32,
 }
 
 
@@ -44,33 +50,32 @@ impl MyApp {
         let mut app = MyApp {
             sections: vec![],
             mode: Mode::Idle,
+            first_time_edit: false,
+            scale_factor: 1.0,
         };
 
-        app.add_section("Graphics");
-        app.add_section("Gameplay");
+        app.add_section("Graphics", false);
+        app.add_section("Gameplay", false);
 
-        app.add_task("Graphics", "Main character sprite");
-        app.add_task("Graphics", "Lab sprite");
-        app.add_task("Graphics", "Brain sprite");
+        app.add_task("Graphics", "Main character sprite", false);
+        app.add_task("Graphics", "Lab sprite", false);
+        app.add_task("Graphics", "Brain sprite", false);
 
-        app.add_task("Gameplay", "Figure out basic puzzle mechanic");
+        app.add_task("Gameplay", "Figure out basic puzzle mechanic", false);
 
         app
     }
 
-    fn add_section(&mut self, title: &str) {
-        self.sections.push(Section {title: title.to_string(), tasks: vec![], edit: false});
-        println!("Added section {}", title);
+
+    fn add_section(&mut self, title: &str, edit: bool) {
+        self.sections.push(Section {title: title.to_string(), tasks: vec![], edit});
     }
 
-    fn add_task(&mut self, section_title: &str, task: &str) {
+    fn add_task(&mut self, section_title: &str, task: &str, edit: bool) {
         for section in &mut self.sections {
             if section_title == section.title {
-                println!("Added task {}", task);
-                section.tasks.push(Task {text: task.to_string(), done: false, edit: false});
+                section.add_task(task, edit);
                 break;
-            } else {
-                println!("Could not add task {}", task);
             }
         }
     }
@@ -79,36 +84,74 @@ impl MyApp {
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-
             match self.mode {
                 // In this mode you can click a Task or a Section to edit it,
                 // the checkboxes to mark tasks as completed and add new Tasks
                 // and Sections
                 Mode::Idle => {
+                    // Handle zooming
+                    if ui.input(|i| i.key_pressed(egui::Key::ArrowUp)) {
+                        self.scale_factor += 0.2;
+
+                        if self.scale_factor > 3.0 {
+                            self.scale_factor = 3.0;
+                        }
+
+                        ctx.set_pixels_per_point(self.scale_factor);
+                    }
+                    if ui.input(|i| i.key_pressed(egui::Key::ArrowDown)) {
+                        self.scale_factor -= 0.2;
+
+                        if self.scale_factor < 1.0 {
+                            self.scale_factor = 1.0;
+                        }
+
+                        ctx.set_pixels_per_point(self.scale_factor);
+                    }
+
                     for section in &mut self.sections {
+                        // Render Section title as clickable, if clicked edit it
                         if ui.add(Label::new(RichText::new(&section.title).heading()).sense(Sense::click())).clicked() {
                             // Enter edit section mode
                             section.edit = true;
                             self.mode = Mode::EditSection;
                         }
 
+                        // Render Tasks as clickable, if clicked edit it
                         for task in &mut section.tasks {
                             ui.horizontal(|ui| {
                                 ui.checkbox(&mut task.done, "");
                                 if ui.add(Label::new(&task.text).sense(Sense::click())).clicked() {
                                     task.edit = true;
                                     self.mode = Mode::EditTask;
+                                    self.first_time_edit = true;
                                 }
                             });
                         }
 
-                        ui.add_space(12.0);
+                        // Render an invisible Task used to add a Task
+                        let response = ui.add(Label::new("                             "));
+                        if response.clicked() {
+                            let empty = String::new();
+                            section.add_task(&empty, true);
+                            self.mode = Mode::EditTask;
+                            self.first_time_edit = true;
+                        }
+                    }
+
+                    // Render an invisible Section used to add a Section
+                    let response = ui.add(Label::new(RichText::new("                            ").heading()));
+                    if response.clicked() {
+                        let empty = String::new();
+                        self.add_section(&empty, true);
+                        self.mode = Mode::EditSection;
+                        self.first_time_edit = true;
                     }
                 },
 
                 // In this mode all Sections and Tasks are rendered as plain
                 // labels except for the Task being edited which should be an
-                // edit box
+                // edit box. This mode is also entered when a new task is added
                 Mode::EditTask => {
                     for section in &mut self.sections {
                         ui.heading(&section.title);
@@ -120,7 +163,12 @@ impl eframe::App for MyApp {
                                     // Render edit text box for task
                                     let response = ui.add(TextEdit::singleline(&mut task.text));
 
-                                    if response.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter)) {
+                                    if self.first_time_edit {
+                                        response.request_focus();
+                                        self.first_time_edit = false;
+                                    }
+
+                                    if response.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter) || i.key_pressed(egui::Key::Escape)) {
                                         self.mode = Mode::Idle;
                                     }
                                 } else {
@@ -143,7 +191,12 @@ impl eframe::App for MyApp {
                         if section.edit {
                             let response = ui.add(TextEdit::singleline(&mut section.title));
 
-                            if response.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter)) {
+                            if self.first_time_edit {
+                                response.request_focus();
+                                self.first_time_edit = false;
+                            }
+
+                            if response.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter) || i.key_pressed(egui::Key::Escape)) {
                                 self.mode = Mode::Idle;
                             }
                         } else {
@@ -159,12 +212,6 @@ impl eframe::App for MyApp {
 
                         ui.add_space(12.0);
                     }
-                },
-
-                Mode::AddTask => {
-                },
-
-                Mode::AddSection => {
                 },
             }
         });
